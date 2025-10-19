57ebf709e03fc5192257384cb5a02631436fc2fe
Rei-sun <Rei-sun@users.noreply.github.com>
2025-10-19 12:53:42 +0900

    Refactor gz.launch.py to improve formatting of launch arguments for gz_sim

--- core_gz/launch/gz.launch.py
+++ core_gz/launch/gz.launch.py
@@ -55,7 +55,12 @@ def generate_launch_description():
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
-        launch_arguments={'gz_args': [LaunchConfiguration('world')]}.items(),
+        launch_arguments={
+            'gz_args': [
+                '-r ',
+                LaunchConfiguration('world')
+            ]
+        }.items(),
     )
 
     spawn_robot = Node(
