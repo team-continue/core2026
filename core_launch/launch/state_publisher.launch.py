6299edaecb443b12eed10c6b64ab3a54e4f18006
curious-jp <curious.ks.jp@gmail.com>
2025-11-15 23:45:37 +0900

    Remove unused imports in state_publisher.launch.py for cleaner code

--- core_launch/launch/state_publisher.launch.py
+++ core_launch/launch/state_publisher.launch.py
@@ -1,10 +1,8 @@
-import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
 
